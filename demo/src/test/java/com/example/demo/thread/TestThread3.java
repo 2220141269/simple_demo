678d3f8829e5a81ba70e66343c24b3242966ab6a
liaocao <2220141269@qq.com>
2020-05-09 15:54:38 +0800

    测试多线程让cpu满负荷

--- demo/src/test/java/com/example/demo/thread/TestThread3.java
+++ demo/src/test/java/com/example/demo/thread/TestThread3.java
@@ -8,14 +8,14 @@ public class TestThread3 extends  Thread{
         TestThread3 thead1 = new TestThread3();
         TestThread3 thead2 = new TestThread3();
         TestThread3 thead3 = new TestThread3();
-        /*TestThread3 thead4 = new TestThread3();
-        TestThread3 thead5 = new TestThread3();*/
+        TestThread3 thead4 = new TestThread3();
+        TestThread3 thead5 = new TestThread3();
 
         thead1.start();
         thead2.start();
         thead3.start();
-        /*thead4.start();
-        thead5.start();*/
+        thead4.start();
+        thead5.start();
 
 
     }
